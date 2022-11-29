90dff5d1b09b007d24b4951b372d451bfa22c116
Kui-Feng Lee <kuifeng@fb.com>
2022-11-28 17:40:18 -0800

    Parse the line table of GSYM files.
    The line table of GSYM file provide a way to translate an address to
    the file name and the line number of its source code if there is.
    
    Signed-off-by: Kui-Feng Lee <kuifeng@fb.com>

--- src/gsym.rs
+++ src/gsym.rs
@@ -5,10 +5,13 @@ use std::path::{Path, PathBuf};
 
 use super::{AddressLineInfo, FindAddrOpts, SymResolver, SymbolInfo};
 
+mod linetab;
 mod parser;
 mod types;
 
-use parser::{find_address, GsymContext};
+use linetab::{line_table_row_from, run_op, RunResult};
+use parser::{find_address, parse_address_data, parse_line_table_header, GsymContext};
+use types::InfoTypeLineTableInfo;
 
 /// The symbol resolver for the GSYM format.
 pub struct GsymResolver {
@@ -74,6 +77,82 @@ impl SymResolver for GsymResolver {
         None
     }
 
+    fn find_line_info(&self, addr: u64) -> Option<AddressLineInfo> {
+        let addr = addr - self.loaded_address;
+        let idx = find_address(&self.ctx, addr);
+        let symaddr = self.ctx.addr_at(idx);
+        if addr < symaddr {
+            return None;
+        }
+        let addrinfo = self.ctx.addr_info(idx);
+        if addr >= (symaddr + addrinfo.size as u64) {
+            return None;
+        }
+
+        let addrdatas = parse_address_data(addrinfo.data);
+        for addrdata in addrdatas {
+            if addrdata.typ != InfoTypeLineTableInfo {
+                continue;
+            }
+            let lthdr = parse_line_table_header(addrdata.data);
+            if lthdr.is_err() {
+                #[cfg(debug_assertions)]
+                eprintln!("invalid line table header");
+                return None;
+            }
+            let (lthdr, bytes) = lthdr.unwrap();
+            let ops = &addrdata.data[bytes..];
+            let mut ltr = line_table_row_from(&lthdr, symaddr);
+            let mut saved_ltr = ltr.clone();
+            let mut row_cnt = 0;
+            let mut pc = 0;
+            while pc < ops.len() {
+                match run_op(&mut ltr, &lthdr, ops, pc) {
+                    RunResult::Ok(bytes) => {
+                        pc += bytes as usize;
+                    }
+                    RunResult::NewRow(bytes) => {
+                        pc += bytes as usize;
+                        row_cnt += 1;
+                        if addr == ltr.address {
+                            break;
+                        }
+                        if addr < ltr.address {
+                            if row_cnt == 1 {
+                                return None;
+                            }
+                            ltr = saved_ltr.clone();
+                            break;
+                        }
+                        saved_ltr = ltr.clone();
+                    }
+                    RunResult::End | RunResult::Err => {
+                        break;
+                    }
+                }
+            }
+
+            if row_cnt == 0 {
+                continue;
+            }
+
+            let finfo = self.ctx.file_info(ltr.file_idx as usize);
+            let dirname = self.ctx.get_str(finfo.directory as usize);
+            let filename = self.ctx.get_str(finfo.filename as usize);
+            let path = Path::new(dirname)
+                .join(filename)
+                .to_str()
+                .unwrap()
+                .to_string();
+            return Some(AddressLineInfo {
+                path,
+                line_no: ltr.file_line as usize,
+                column: 0,
+            });
+        }
+        None
+    }
+
     fn addr_file_off(&self, _addr: u64) -> Option<u64> {
         // Unavailable
         None
@@ -83,11 +162,61 @@ impl SymResolver for GsymResolver {
         &self.file_name
     }
 
-    fn find_line_info(&self, _addr: u64) -> Option<AddressLineInfo> {
-        None
-    }
-
     fn repr(&self) -> String {
         format!("GSYM {:?}", self.file_name)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn test_find_line_info() {
+        let args: Vec<String> = env::args().collect();
+        let bin_name = &args[0];
+        let test_gsym = Path::new(bin_name)
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .join("data")
+            .join("test.gsym");
+        let resolver = GsymResolver::new(test_gsym, 0).unwrap();
+
+        let linfo = resolver.find_line_info(0x0000000002000001);
+        assert!(linfo.is_some());
+        let linfo = linfo.unwrap();
+        assert_eq!(linfo.line_no, 54);
+        assert!(linfo.path.ends_with("gsym-example.c"));
+    }
+
+    #[test]
+    fn test_find_symbols() {
+        let args: Vec<String> = env::args().collect();
+        let bin_name = &args[0];
+        let test_gsym = Path::new(bin_name)
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .join("data")
+            .join("test.gsym");
+        let resolver = GsymResolver::new(test_gsym, 0).unwrap();
+
+        let syms = resolver.find_symbols(0x0000000002020000);
+        assert_eq!(syms.len(), 1);
+        let (name, addr) = syms[0];
+        assert_eq!(name, "factorial");
+        assert_eq!(addr, 0x0000000002020000);
     }
 }
